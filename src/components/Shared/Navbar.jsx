98ff6a0baaf9d2232ec77c5187b75190cdb26e47
nibirshahriar <nibir1891@gmail.com>
2026-04-15 21:29:59 +0600

    added navbar, and react router in nav

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import LOGO from "../../assets/images/logo.png";
 import { Link } from "react-router";
+import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav>
-      <img src={LOGO} alt="" className="w-[50px]" />
-      <ul>
-        <li>
+    <nav className="shadow">
+      <div className="flex justify-between items-center gap-4 bg-white py-3 container mx-auto">
+        <img src={LOGO} alt="" className="w-[50px]" />
+        <ul className="flex justify-between items-center gap-4">
+          <li>
+            {" "}
+            <Link to={"/"}>Home</Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <Link to={"/apps"}>Apps</Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <Link to={"/installapps"}>Installation</Link>{" "}
+          </li>
+        </ul>
+        <button className="btn bg-purple-500 text-white">
           {" "}
-          <Link>Home</Link>{" "}
-        </li>
-        <li>
-          {" "}
-          <Link>Apps</Link>{" "}
-        </li>
-        <li>
-          {" "}
-          <Link>Installation</Link>{" "}
-        </li>
-      </ul>
-      <button>Contribute</button>
+          <FaGithub /> Contribute
+        </button>
+      </div>
     </nav>
   );
 };
